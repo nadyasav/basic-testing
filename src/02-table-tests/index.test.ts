410121786bb184068c341daa5048b31a4da6930a
nadyasav <nadyasav28@gmail.com>
2025-05-13 00:23:16 +0300

    fix(02-table-tests): add test case for invalid action

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,10 +1,9 @@
 import { simpleCalculator, Action } from './index';
 
 const testCases = [
-  { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
   { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 3, b: '2', action: Action.Add, expected: null },
+  { a: 3, b: 2, action: '%', expected: null },
   { a: 3, b: 2, action: Action.Subtract, expected: 1 },
   { a: 3, b: 2, action: Action.Multiply, expected: 6 },
   { a: 3, b: -2, action: Action.Divide, expected: -1.5 },
